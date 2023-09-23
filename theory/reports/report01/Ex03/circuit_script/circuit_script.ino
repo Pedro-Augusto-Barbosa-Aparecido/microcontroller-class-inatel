813efd5d9fbf9d3a0c699469fad5894111bef993
Pedro Augusto Barbosa Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2023-09-22 23:00:21 -0300

    Relatorio 01 - Ex03

---
+++ theory/reports/report01/Ex03/circuit_script/circuit_script.ino
@@ -0,0 +1,142 @@
+#include <stdarg.h>
+
+#define MOTOR_TRANSPORTER_TO_ELEVATOR (1 << PD0) // M1
+#define MOTOR_TRANSPORTER_TO_VERIFICATION (1 << PD1) // M2
+#define MOTOR_ELEVATOR (1 << PD2)
+#define SENSOR_ONE (1 << PB0)
+#define SENSOR_TWO (1 << PB1)
+#define SENSOR_THREE (1 << PB2)
+#define SENSOR_FOUR (1 << PB3)
+#define TURN_ON_BUTTON (1 << PC0)
+#define TURN_OFF_BUTTON (1 << PC1)
+#define LAMP (1 << PD3)
+
+void turnOn(int qtdPins, ...) {
+  int pinsToTurnOn = 0b00000000;
+
+  va_list pins;
+  va_start(pins, qtdPins);
+
+  for (short int i = 0; i < qtdPins; i++) {
+    pinsToTurnOn += va_arg(pins, int);
+  }
+
+  va_end(pins);
+
+  PORTD |= pinsToTurnOn;
+}
+
+void turnOff(int qtdPins, ...) {
+  int pinsToTurnOff = 0b00000000;
+
+  va_list pins;
+  va_start(pins, qtdPins);
+
+  for (short int i = 0; i < qtdPins; i++) {
+    pinsToTurnOff += va_arg(pins, int);
+  }
+
+  va_end(pins);
+
+  PORTD &= ~pinsToTurnOff;
+}
+
+void blinkLamp() {
+  turnOn(1, LAMP);
+
+  _delay_ms(500);
+
+  turnOff(1, LAMP);
+}
+
+int main() {
+  // define outputs
+  DDRD = MOTOR_ELEVATOR + MOTOR_TRANSPORTER_TO_ELEVATOR + MOTOR_TRANSPORTER_TO_VERIFICATION + LAMP;
+
+  // pull up on turn off button
+  PORTC = TURN_OFF_BUTTON;
+
+  // set outputs as turned off by default
+  turnOff(
+    4,
+    MOTOR_ELEVATOR, 
+    MOTOR_TRANSPORTER_TO_ELEVATOR, 
+    MOTOR_TRANSPORTER_TO_VERIFICATION,
+    LAMP
+  );
+
+  bool isSystemRunning = false;
+
+  for (;;) {
+    short int isTurnOnButtonPressed = PINC & TURN_ON_BUTTON;
+    short int isTurnOffButtonPressed = PINC & TURN_OFF_BUTTON;
+    short int isSensorOneActivated = PINB & SENSOR_ONE;
+    short int isSensorTwoActivated = PINB & SENSOR_TWO;
+    short int isSensorThreeActivated = PINB & SENSOR_THREE;
+    short int isSensorFourActivated = PINB & SENSOR_FOUR;
+
+    if (isTurnOnButtonPressed == TURN_ON_BUTTON) {
+      turnOn(
+        1,
+        MOTOR_TRANSPORTER_TO_ELEVATOR
+      );
+
+      isSystemRunning = true;
+    } else if (isTurnOffButtonPressed == 0) {
+      turnOff(
+        4,
+        MOTOR_ELEVATOR, 
+        MOTOR_TRANSPORTER_TO_ELEVATOR, 
+        MOTOR_TRANSPORTER_TO_VERIFICATION,
+        LAMP
+      );
+
+      isSystemRunning = false;
+    } else if (isSensorOneActivated == SENSOR_ONE && isSystemRunning) {
+      turnOff(
+        1,
+        MOTOR_TRANSPORTER_TO_ELEVATOR
+      );
+
+      _delay_ms(3000);
+
+      turnOn(
+        1,
+        MOTOR_ELEVATOR
+      );
+    } else if (isSensorTwoActivated == SENSOR_TWO && isSystemRunning) {
+      turnOff(
+        1,
+        MOTOR_ELEVATOR
+      );
+
+      turnOn(
+        1,
+        MOTOR_TRANSPORTER_TO_VERIFICATION
+      );
+    } else if (isSensorThreeActivated == SENSOR_THREE && isSystemRunning) {
+      turnOff(
+        1,
+        MOTOR_TRANSPORTER_TO_VERIFICATION
+      );
+
+      _delay_ms(3000);
+
+      turnOn(
+        1,
+        MOTOR_TRANSPORTER_TO_VERIFICATION
+      );
+
+      while ((PINB & SENSOR_THREE) == SENSOR_THREE) 
+        blinkLamp();
+    } else if (isSensorFourActivated == SENSOR_FOUR && isSystemRunning) {
+      turnOff(
+        4,
+        MOTOR_ELEVATOR, 
+        MOTOR_TRANSPORTER_TO_ELEVATOR, 
+        MOTOR_TRANSPORTER_TO_VERIFICATION,
+        LAMP
+      );
+    }
+  }
+}
